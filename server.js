e08c9cdd0afede806a4b65854841e9022108610c
Miko <89363492+MikoGome@users.noreply.github.com>
2024-07-02 13:35:32 -0700

    cors

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-})
+app.use(cors());
 
 app.get('/:status', (req, res) => {
   res.sendStatus(req.params.status);
